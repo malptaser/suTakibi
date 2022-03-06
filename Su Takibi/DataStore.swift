5cf0e5a89786e5a2051977f7d3d831d29cc7f6c1
M.Alp <mehmetalp.taser@bahcesehir.edu.tr>
2022-03-06 22:05:54 +0300

    starterPack

---
+++ Su Takibi/DataStore.swift
@@ -0,0 +1,18 @@
+//
+//  DataStore.swift
+//  Water Tracker
+//
+//  Created by Can Balkaya on 3/27/20.
+//  Copyright © 2020 TurkishKit. All rights reserved.
+//
+
+import Foundation
+
+class DataStore {
+    
+    // MARK: - Properties
+    
+    // MARK: - Life Cycle
+    
+    // MARK: - Functions
+}
---
+++ Su Takibi/WaterTrackerController.swift
@@ -0,0 +1,23 @@
+//
+//  WaterTracker.swift
+//  Water Tracker
+//
+//  Created by Can Balkaya on 3/25/20.
+//  Copyright © 2020 TurkishKit. All rights reserved.
+//
+
+import UIKit
+
+class WaterTrackerController: UIViewController {
+    
+    // MARK: - UI Elements
+    
+    // MARK: - Properties
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+}
