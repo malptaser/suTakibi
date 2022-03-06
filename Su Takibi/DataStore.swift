6803779b7ca1981d45d35044f7bbf6f591ceeaf9
M.Alp <mehmetalp.taser@bahcesehir.edu.tr>
2022-03-07 00:20:16 +0300

    tamamlandı

--- Su Takibi/DataStore.swift
+++ Su Takibi/DataStore.swift
@@ -12,7 +12,53 @@ class DataStore {
     
     // MARK: - Properties
     
+    let defaults = UserDefaults.standard
+
+    
+    var currentDay: Int {
+        let now = Date ()
+        let day = Calendar.current.component(.day, from: now)
+
+        return day
+    }
+
+    
     // MARK: - Life Cycle
     
+    init() {
+        let latestsUpdateDay = getLatestUpdateDay()
+        
+        if latestsUpdateDay != currentDay {
+            defaults.set(0, forKey: "currentAmount")
+        }
+    }
+    
+    
     // MARK: - Functions
+
+    func getCurrentAmount()  -> Double {
+        let savedAmount = defaults.double(forKey: "currentAmount")
+    
+    
+    return savedAmount
+    }
+
+
+    func getLatestUpdateDay() -> Int {
+        let latestDay = defaults.integer(forKey: "latestUpdateDay")
+    
+    return latestDay
+    }
+
+    func addWater(amount: Double) {
+        let currentAmount = getCurrentAmount()
+ let newAmount = currentAmount + amount
+        
+        defaults.set(newAmount, forKey: "currentAmount")
+                     defaults.set(currentDay, forKey: "latestUpdateDay")
+                     
+
+
+}
+
 }
--- Su Takibi/WaterTrackerController.swift
+++ Su Takibi/WaterTrackerController.swift
@@ -18,19 +18,80 @@ class WaterTrackerController: UIViewController {
     
     // MARK: - Properties
     
+    let waterStore = DataStore()
+    let targetAmount = 2700.0
+    
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateApperance()
+    }
+    // MARK: - Functions
+    
+    func updateWaterLevel(amount: Double) {
+        let screenHeight = Double(view.frame.size.height)
+        let ratio = amount / targetAmount
+        let calculatedHeight = screenHeight * ratio
+        
+        
+        addWaterConstraint.constant = CGFloat(calculatedHeight)
+        
+        UIViewPropertyAnimator.init(duration: 0.5, dampingRatio: 0.75) {
+            self.view.layoutIfNeeded()
+        }.startAnimation()
     }
     
+    func updateLabels(amount: Double) {
+        let amountToTarget = (targetAmount - amount) / 1000
+        if amount < targetAmount {
+            let subtitleText = String(format: "Bugünkü ihtiyacını karşılamak için \n%g litre daha su içmelisin", amountToTarget)
+            
+            subtitleLabel.text = subtitleText
+            
+            if amount == 0 {
+                titleLabel.text = "Merhaba ! \nBugün su içtin mi?"
+            } else {
+                titleLabel.text = "Tebrikler! \nDoğru yoldasın."
+            }
+        } else {
+            titleLabel.text = "Muhteşem! \nKendine iyi baktın."
+            subtitleLabel.text = "Bugün vücudun için gereken su miktarının tamamını karşıladın."
+    }
+    
+}
+    
+    func updateApperance() {
+        let currentWaterAmount = waterStore.getCurrentAmount()
+        
+        updateLabels(amount : currentWaterAmount)
+        updateWaterLevel(amount: currentWaterAmount)
+    }
     // MARK: - Actions
-
-    @IBAction func firstButton(_ sender: Any) {
+    
+    
+    @IBAction func addWaterButtonTapped(_ sender: Any) {
+        
+        var waterAmount = 0.0
+        
+        switch (sender as AnyObject).tag {
+        
+        case 0:
+            waterAmount = 200
+            
+        case 1:
+            waterAmount = 500
+            
+        case 2:
+            waterAmount = 800
+        default:
+            break
+        }
+        
+        
+        waterStore.addWater(amount: waterAmount)
+        updateApperance()
     }
     
-    @IBAction func secondButton(_ sender: Any) {
-    }
     
-    @IBAction func thirdButton(_ sender: Any) {
-    }
 }
